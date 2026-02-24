6a179cfd5e83361c4145b78908c49c3cfef64728
Gagan sen <153493491+gagan0097sen@users.noreply.github.com>
2026-02-25 03:04:20 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://k4media-image-gallery.onrender.com" || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -46,3 +46,4 @@ export const imageService = {
 };
 
 export default api;
+
